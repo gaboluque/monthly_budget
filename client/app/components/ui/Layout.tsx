5739c6ee0bc3a570d96d87774392daa6cc3a2acb
Gabriel Luque <gluquemiranda@gmail.com>
2025-03-20 14:06:56 -0500

    chore: remove fab in transactions page

--- client/app/components/ui/Layout.tsx
+++ client/app/components/ui/Layout.tsx
@@ -3,11 +3,15 @@ import { Header } from './Header';
 import { MobileNavigation } from './MobileNavigation';
 import ProtectedRoute from '../ProtectedRoute';
 import { FloatingActionButton } from './FloatingActionButton';
+import { useLocation } from '@remix-run/react';
+
 interface LayoutProps {
   children: ReactNode;
 }
 
 export function Layout({ children }: LayoutProps) {
+  const location = useLocation();
+  const showTransactionFAB = location.pathname !== '/transactions';
 
   return (
     <ProtectedRoute>
@@ -16,10 +20,12 @@ export function Layout({ children }: LayoutProps) {
         <main className="container mx-auto px-4 sm:px-6 lg:px-8 py-6 md:max-w-4xl pb-40 md:pb-6">
           {children}
         </main>
-        <FloatingActionButton
-          to="/transactions?new=true"
-          ariaLabel="Add new transaction"
-        />
+        {showTransactionFAB && (
+          <FloatingActionButton
+            to="/transactions?new=true"
+            ariaLabel="Add new transaction"
+          />
+        )}
         <MobileNavigation />
       </div>
     </ProtectedRoute>
