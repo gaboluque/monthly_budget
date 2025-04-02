c10c4a4eaec26045708af50f76e7092d7242a93d
Gabriel Luque <gluquemiranda@gmail.com>
2025-04-02 17:16:48 -0500

    chore: hosts

--- server/config/environments/development.rb
+++ server/config/environments/development.rb
@@ -70,5 +70,7 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
 
+  config.hosts << "monthly-budget-dev-eyzxa.ondigitalocean.app"
+
   STDOUT.sync = true
 end
