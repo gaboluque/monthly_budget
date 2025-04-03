89c95536910ec6d698a5c9cb1c0d0efd6e2ea0f9
Gabriel Luque <gluquemiranda@gmail.com>
2025-04-03 16:54:30 -0500

    chore: process env

--- client/app/lib/utils/getEnv.ts
+++ client/app/lib/utils/getEnv.ts
@@ -1,6 +1,11 @@
 
 export const getEnv = () => {
   console.log("getEnv", process.env.BASE_API_URL);
+
+  if (typeof window !== "undefined") {
+    console.log("getEnv", window.process.env.BASE_API_URL);
+  }
+
   return {
     BASE_API_URL: process.env.BASE_API_URL,
   };
